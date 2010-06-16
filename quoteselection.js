2e8c6d9f5bdc06149f3ebfd6f84ffb5f4924f263
Igor Tarasov <tarasov.igor@gmail.com>
2010-06-16 14:06:04 +0400

    Fix js for IE.

--- quoteselection.js
+++ quoteselection.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
     quoteData = {};
     // we use "mouseup" event since Chrome does not work with "select" one.
-    $("ul.Discussion div.Comment").mouseup(function(e){
+    $("body").mouseup(function(e){
         if (txt = window.getSelection)
             txt = window.getSelection().toString();
         else
             txt = document.selection.createRange().text;
-        quoteButton = getQuoteButton(e);
         quoteData = {};
+        quoteButton = getQuoteButton(e);
         if (!txt)
             // no text selected.
             return;
 
-        meta = $(e.currentTarget).find(".Meta");
-        name = meta.find(".Author").text().trim();
-        url = meta.find(".Permalink a").attr("href");
-    
-        quoteData = {text: txt, name:name, url:url}
+        meta = $(e.target).parents("li.Comment").find(".Meta");
+        if (meta.length==0) 
+            // selected text does not belong to comments
+            return;
+        quoteData = {
+            text: txt,
+            author: trim(meta.find(".Author").text()),
+            url: meta.find(".Permalink a").attr("href")
+        }
         quoteButton.show();
     });
 
-    // Creates or returns (if it already exists) "Quote" button, and sets it's position
+    // Creates or returns (if it already exists) "Quote" button, sets it's position. and hides it if it was displayed.
     getQuoteButton = function(e) {
         if (!document.getElementById("quoteButton")) {
             quoteButton = document.createElement("a");
@@ -28,7 +32,7 @@ $(document).ready(function() {
             quoteButton.id = "quoteButton";
             quoteButton.href = "#Form_Comment";
             document.body.insertBefore(quoteButton, document.body.firstChild);
-            $("#quoteButton").text(gdn.definition("qsQuote")).click(function(e){
+            $("#quoteButton").text(gdn.definition("qsQuote")).mouseup(function(e){
                 body = $("#Form_Body").val();
                 if (body!='') {
                     body += "\n\n";
@@ -36,6 +40,7 @@ $(document).ready(function() {
                 resetCommentForm();
                 $("#Form_Body").val(body + getQuoteText()).focus();
                 $("#quoteButton").hide();
+                return false;
             });
         }
         return $("#quoteButton").css({display:"none",position:"absolute",left:e.pageX,top:e.pageY});
@@ -43,17 +48,17 @@ $(document).ready(function() {
 
     // returns quotation formatted according current InputFormatter setting
     getQuoteText = function() {
-        title = gdn.definition('qsQuoteText').replace('%s', quoteData.name);
+        title = gdn.definition('qsQuoteText').replace('%s', quoteData.author);
         // trim down domain part to make links shorter
         url = quoteData.url.replace(/^https?:\/\/[^\/]*/, '');
         switch (gdn.definition('qsInputFormatter','Html')) {
             case "Markdown":
-                return "> *[" + title + "](" + url + ")*  \n" + quoteData.text.replace("\r", "").replace(/\n{2,}/gm, "\n\n>") + "\n\n";
+                return "> [" + title + "](" + url + "): " + quoteData.text.replace("\r", "").replace(/\n{2,}/gm, "\n\n>") + "\n\n";
             case "BBCode":
-                return "[quote][i][url=" + url + "]" + title + "[/url][/i]\n" + quoteData.text + "[/quote]\n\n";
+                return "[quote][url=" + url + "]" + title + "[/url]: " + quoteData.text + "[/quote]\n\n";
                 break
             default:
-                return "<blockquote><i><a href=\"" + url + "\">" + title + "</a></i>\n" + quoteData.text + "</blockquote>\n\n";
+                return "<blockquote><a href=\"" + url + "\">" + title + "</a>: " + quoteData.text + "</blockquote>\n\n";
                 break;
         }
     }
@@ -66,5 +71,12 @@ $(document).ready(function() {
         $(parent).find('div.Preview').remove();
         $(parent).find('textarea').show();
         $('span.TinyProgress').remove();
+    }
+    
+    trim = function(str) {
+        if (typeof str.trim == "function")
+            return str.trim();
+         // IE is not aware of trim function, so we have to use this lame stuff
+        return str.replace(/^\s+/g,'').replace(/\s+$/g,'');
     }
 });
