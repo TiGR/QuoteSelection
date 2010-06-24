9aae40533d6e132404c569c3eaa3662fc00fbf2a
Igor Tarasov <tarasov.igor@gmail.com>
2010-06-24 20:53:39 +0400

    Rewrite JS to make it reusable.

--- quoteselection.js
+++ quoteselection.js
@@ -1,40 +1,45 @@
-$(document).ready(function() {
-    quoteData = {};
-    // we use "mouseup" event since Chrome does not work with "select" one.
-    $("body").mouseup(function(e){
-        if (txt = window.getSelection)
-            txt = window.getSelection().toString();
-        else
-            txt = document.selection.createRange().text;
-        quoteData = {};
-        quoteButton = getQuoteButton(e);
-        if (!txt)
-            // no text selected.
-            return;
+quoteSelection = {
 
-        meta = $(e.target).parents("li.Comment").find(".Meta");
-        if (meta.length==0) 
-            // selected text does not belong to comments
-            return;
-        quoteData = {
-            text: txt,
-            author: trim(meta.find(".Author").text()),
-            url: meta.find(".Permalink a").attr("href")
-        }
-        quoteButton.show();
-    });
+    quoteData: {},
+    quoteButton: null,
     
-    // Replace absolute comment links with relative where possible
-    $("ul.Discussion div.Message blockquote a").each(function(i,el) {
-        if (el.href.indexOf(gdn.definition("WebRoot") + "/discussion/comment/") === 0) {
-            if ($(el.hash).length == 1) {
-                el.href = el.hash;
+    init: function() {
+        // we use "mouseup" event since Chrome does not work with "select" one.
+        $("body").mouseup(function(e){
+            if (txt = window.getSelection)
+                txt = window.getSelection().toString();
+            else
+                txt = document.selection.createRange().text;
+            quoteData = {};
+            quoteButton = quoteSelection.getQuoteButton(e);
+            if (!txt)
+                // no text selected.
+                return;
+
+            meta = $(e.target).parents("li.Comment").find(".Meta");
+            if (meta.length==0) 
+                // selected text does not belong to comments
+                return;
+            quoteData = {
+                text: txt,
+                author: quoteSelection.trim(meta.find(".Author").text()),
+                url: meta.find(".Permalink a").attr("href")
             }
-        }
-    });
+            quoteButton.show();
+        });
+        
+        // Replace absolute comment links with relative where possible
+        $("ul.Discussion div.Message blockquote a").each(function(i,el) {
+            if (el.href.indexOf(gdn.definition("WebRoot") + "/discussion/comment/") === 0) {
+                if ($(el.hash).length == 1) {
+                    el.href = el.hash;
+                }
+            }
+        });
+    },
 
     // Creates or returns (if it already exists) "Quote" button, sets it's position. and hides it if it was displayed.
-    getQuoteButton = function(e) {
+    getQuoteButton: function(e) {
         if (!document.getElementById("quoteButton")) {
             quoteButton = document.createElement("a");
             quoteButton.style.display = "none";
@@ -46,17 +51,17 @@ $(document).ready(function() {
                 if (body!='') {
                     body += "\n\n";
                 }
-                resetCommentForm();
-                $("#Form_Body").val(body + getQuoteText()).focus();
+                quoteSelection.resetCommentForm();
+                $("#Form_Body").val(body + quoteSelection.getQuoteText()).focus();
                 $("#quoteButton").hide();
                 return false;
             });
         }
         return $("#quoteButton").css({display:"none",position:"absolute",left:e.pageX,top:e.pageY});
-    }
+    },
 
     // returns quotation formatted according current InputFormatter setting
-    getQuoteText = function() {
+    getQuoteText: function() {
         title = gdn.definition('qsQuoteText').replace('%s', quoteData.author);
         switch (gdn.definition('qsInputFormatter','Html')) {
             case "Markdown":
@@ -70,22 +75,26 @@ $(document).ready(function() {
                 return "<blockquote><a href=\"" + quoteData.url + "\">" + title + "</a>: " + quoteData.text + "</blockquote>\n\n";
                 break;
         }
-    }
+    },
     
     // shows Comment textarea if it is not already displayed.
-    function resetCommentForm() {
+    resetCommentForm: function () {
         var parent = $('div.CommentForm');
         $(parent).find('li.Active').removeClass('Active');
         $('a.WriteButton').parents('li').addClass('Active');
         $(parent).find('div.Preview').remove();
         $(parent).find('textarea').show();
         $('span.TinyProgress').remove();
-    }
-    
-    trim = function(str) {
+    },
+
+    trim: function(str) {
         if (typeof str.trim == "function")
             return str.trim();
          // IE is not aware of trim function, so we have to use this lame stuff
         return str.replace(/^\s+/g,'').replace(/\s+$/g,'');
     }
+}
+
+$(document).ready(function() {
+    quoteSelection.init();
 });
