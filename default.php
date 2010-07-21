4fe2d821f5fc54166f58d2e7ad4d1bd4eb43de2c
Igor Tarasov <tarasov.igor@gmail.com>
2010-07-21 16:06:11 +0400

    Make use of new Gdn_Plugin features.

--- default.php
+++ default.php
@@ -5,12 +5,13 @@ $PluginInfo['QuoteSelection'] = array(
     'Name' => 'Quote selection',
     'Description' => "Provides a simple tool to quote posts. Users can select text some comment text and click &quot;quote&quot; button that would appear te get formatted quote.",
     'Version' => '0.5',
+    'HasLocale' => TRUE,
     'Author' => "Igor Tarasov",
     'AuthorEmail' => 'tarasov.igor@gmail.com',
     'AuthorUrl' => 'http://polosatus.ru',
 );
 
-class QuoteSelection implements Gdn_IPlugin {
+class QuoteSelectionPlugin extends Gdn_Plugin {
 
     public function DiscussionController_Render_Before(&$Sender) {
         $Session = Gdn::Session();
@@ -19,8 +20,8 @@ class QuoteSelection implements Gdn_IPlugin {
             // we enable this feature only for logged in users and only on discussion page
             return;
 
-        $Sender->AddJsFile('/plugins/QuoteSelection/quoteselection.js');
-        $Sender->AddCssFile('plugins/QuoteSelection/quoteselection.css');
+        $Sender->AddJsFile($this->GetResource('quoteselection.js', FALSE, FALSE));
+        $Sender->AddCssFile($this->GetResource('quoteselection.css', FALSE, FALSE));
         $Sender->AddDefinition('qsInputFormatter', strtolower(Gdn::Config('Garden.InputFormatter')));
         $Sender->AddDefinition("qsQuote", T('Quote'));
         $Sender->AddDefinition("qsQuoteText", T('%s said'));
